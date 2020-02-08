3400965d34013c0565c84de280225b4a8772d8c7
Bijay Baniya <mt_bij@yahoo.com>
2020-02-08 10:20:10 +0545

    static foldername fix statics ==> static

--- HQTrivia/HQTrivia/settings.py
+++ HQTrivia/HQTrivia/settings.py
@@ -134,5 +134,5 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'statics'),
+    os.path.join(BASE_DIR, 'static'),
 ]
